c2470b1b354d7215f39b7ec8daf153aa1ff58805
hosua <hosua.o@gmail.com>
2021-12-18 05:55:39 -0500

    Added LHS orientation

--- Draw.h
+++ Draw.h
@@ -1,11 +1,12 @@
+// Ascii art made by ejm98 https://www.asciiart.eu/miscellaneous/playing-cards
 #include <string>
 #include <iostream>
 #include "Cards.h"
 
 using namespace std;
 
-// Ascii art made by ejm98 https://www.asciiart.eu/miscellaneous/playing-cards
-static const int CARD_HEIGHT = 6;
+// The number of elements in the string arrays
+static const int CARD_HEIGHT = 6; 
 string spadeCard[] = {
 " _____ ",
 "|Z .  |",
@@ -58,16 +59,16 @@ class DrawHandler{
 	public:
 		// Draws an array of cards in ASCII
 		// Cards are fully displayed and not on top of each other.
-		void drawCards(vector<Card> cardVect, int numCards=-1, int facedown=0){ 			
+		void drawCards(vector<Card> cardVect, int numDrawn=-1, int facedown=0){ 			
 			// Bound checks
-			if (numCards < 0 || numCards > cardVect.size()) numCards = cardVect.size(); 		
+			if (numDrawn < 0 || numDrawn > cardVect.size()) numDrawn = cardVect.size(); 		
 			if (facedown < 0) facedown = 0;
 			if (facedown > cardVect.size()) facedown = cardVect.size();
 			string outStr;
-			// By default numCards will just be the vector size
-			if (numCards == -1) numCards = cardVect.size(); 		
+			// By default numDrawn will just be the vector size
+			if (numDrawn == -1) numDrawn = cardVect.size(); 		
 			for (int i = 0; i < CARD_HEIGHT; i++){
-				for (int j = 0; j < numCards-facedown; j++){
+				for (int j = 0; j < numDrawn-facedown; j++){
 					Card c = cardVect[j];
 					string cardStr = drawMap.find(c.getSuit())->second[i];
 					char ch;
@@ -101,27 +102,27 @@ class DrawHandler{
 				}
 				outStr += "\n";
 			}		
-			cout << outStr;
+			cout << outStr << endl;
 		}
 		// Won't display any face down cards.
 		// Like drawCards, except the cards will be drawn as if they are on top of each other.
 		// topRHS=true will mean that the top card of the card vector will be on the right side.
-		void drawStacked(vector<Card> cardVect, int numCards=-1, int facedown=0, bool topRHS=true){ // Draws an array of cards in ASCII
+		void drawStacked(vector<Card> cardVect, int numDrawn=-1, int facedown=0, bool topRHS=true){ // Draws an array of cards in ASCII
 			// Bound checks
-			if (numCards < 0 || numCards > cardVect.size()) numCards = cardVect.size(); 		
+			if (numDrawn < 0 || numDrawn > cardVect.size()) numDrawn = cardVect.size(); 		
 			if (facedown < 0) facedown = 0;
 			if (facedown > cardVect.size()) facedown = cardVect.size();
+			if (facedown > numDrawn) facedown = numDrawn;
 			string outStr, cardStr;
 			Card c;
 			char ch;
 			// Right orientation
 			if (topRHS){
-				// By default numCards will just be the vector size
 				for (int i = 0; i < CARD_HEIGHT; i++){
                     for (int j = 0; j < facedown; j++){
                         outStr += facedownCard[i].substr(0,3);
                     }
-					for (int j = 0; j < numCards-facedown; j++){
+					for (int j = 0; j < numDrawn-facedown; j++){
 						c = cardVect[j];
 						cardStr = drawMap.find(c.getSuit())->second[i];
 						cardStr = cardStr.substr(0,3);
@@ -137,8 +138,8 @@ class DrawHandler{
 						outStr += cardStr;
 					}
 					// Draw the last card
-					c = cardVect[numCards-1-facedown];
-					if (facedown == cardVect.size())
+					c = cardVect[numDrawn-1-facedown];
+					if (facedown == cardVect.size() || facedown == numDrawn)
 						cardStr = facedownCard[i].substr(3,6);
 					else 
 						cardStr = drawMap.find(c.getSuit())->second[i].substr(3,6);
@@ -149,6 +150,36 @@ class DrawHandler{
 				cout << outStr << endl;
 			// Left orientation
 			} else {
+				for (int i = 0; i < CARD_HEIGHT; i++){
+					// Draw the first card
+					c = cardVect[0];
+					if (facedown == cardVect.size() || facedown == numDrawn)
+						cardStr = facedownCard[i].substr(0,3);
+					else 
+						cardStr = drawMap.find(c.getSuit())->second[i].substr(0,3);
+					replace(cardStr.begin(), cardStr.end(), 'Z', ch);
+					outStr += cardStr;
+					for (int j = 0; j < numDrawn-facedown; j++){
+						c = cardVect[j];
+						cardStr = drawMap.find(c.getSuit())->second[i];
+						cardStr = cardStr.substr(3,6);
+						// 10 will require a special case because its length is 2.
+						if (c.getRank() != Ten){
+							ch = rankToChar.find(c.getRank())->second;
+							replace(cardStr.begin(), cardStr.end(), 'Z', ch);
+						} else {
+							if (i == 1){
+								cardStr = "|10";
+							}
+						}
+						outStr += cardStr;
+					}
+                    for (int j = 0; j < facedown; j++){
+                        outStr += facedownCard[i].substr(3,6);
+                    }
+					outStr += "\n";
+				}		
+				cout << outStr << endl;
 			}
 		}
 
@@ -178,25 +209,9 @@ class DrawHandler{
 		}
 		cout << outStr << endl;
 	}
-	// Display the 4 card suits with no rank
+	// Draw no stacked
 	void drawTest1(){
 		cout << "Draw test 1" << endl;
-		for (int i = 0; i < CARD_HEIGHT; i++){
-			cout << spadeCard[i] << endl;
-		}
-		for (int i = 0; i < CARD_HEIGHT; i++){
-			cout << heartCard[i] << endl;
-		}
-		for (int i = 0; i < CARD_HEIGHT; i++){
-			cout << diamondCard[i] << endl;
-		}
-		for ( int i = 0; i < CARD_HEIGHT; i++){
-			cout << clubCard[i] << endl;
-		}
-	}
-	// Draw no stacked
-	void drawTest2(){
-		cout << "Draw test 2" << endl;
 		vector<Card> hand;
 		Card c1, c2, c3, c4, c5, c6, c7, c8;
 		c1.setCard(Ace, Spade); 
@@ -216,11 +231,11 @@ class DrawHandler{
 		hand.push_back(c7);
 		hand.push_back(c8);
 		DrawHandler Drawer;
-		Drawer.drawCards(hand);
+		Drawer.drawCards(hand, 6, 4);
 	}
-	// Draw stacked, RHS, with some face down
-	void drawTest3(){
-		cout << "Draw test 3" << endl;
+	// Draw stacked, RHS on top with some cards face down
+	void drawTest2(){
+		cout << "Draw test 2 (drawStacked RHS)" << endl;
 		vector<Card> hand;
 		Card c1, c2, c3, c4, c5, c6, c7, c8;
 		c1.setCard(Ace, Spade); 
@@ -242,7 +257,29 @@ class DrawHandler{
 		DrawHandler Drawer;
 		Drawer.drawStacked(hand, 6, 4);
 	}
-
-
+	// Draw stacked, LHS on top, with some cards face down
+	void drawTest3(){
+		cout << "Draw test 3 (drawStacked LHS)" << endl;
+		vector<Card> hand;
+		Card c1, c2, c3, c4, c5, c6, c7, c8;
+		c1.setCard(Ace, Spade); 
+		c2.setCard(Ace, Heart);	
+		c3.setCard(Ace, Diamond);
+		c4.setCard(Ace, Club);
+		c5.setCard(Ten, Spade);
+		c6.setCard(Ten, Heart);	
+		c7.setCard(Ten, Diamond);
+		c8.setCard(Ten, Club); 
+		hand.push_back(c1);
+		hand.push_back(c2);
+		hand.push_back(c3);
+		hand.push_back(c4);
+		hand.push_back(c5);
+		hand.push_back(c6);
+		hand.push_back(c7);
+		hand.push_back(c8);
+		DrawHandler Drawer;
+		Drawer.drawStacked(hand, 6, 4, false);
+	}
 };
 
--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include "Test.h"
-// #include "War.h"
-
-int main(){
-	//DrawHandler DH;
-	//DH.drawTest2();
-	srand(time(0)); // Always start program with this to set RNG seed based on the time
-	//testProg2();
+void drawTest(){
 	DrawHandler DH;
+	DH.drawTest1();
 	DH.drawTest2();
 	DH.drawTest3();
+}
 
+int main(){
+	srand(time(0)); // Always start program with this to set RNG seed based on the time
+	drawTest();
+	//testProg2();
 }
 
